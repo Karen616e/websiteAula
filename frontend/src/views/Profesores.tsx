cfe5dab508de0c74973d3609214d9d0a492ab530
Fernando Perez <fer658798@gmail.com>
2025-07-19 23:50:00 -0600

    feat: add cards in coordinacion and profesores pages

--- frontend/src/views/Profesores.tsx
+++ frontend/src/views/Profesores.tsx
@@ -1,5 +1,22 @@
+import InformationCard from "../components/InformationCard";
+import professors from "../data/professorsData";
+
 export default function Profesores() {
+  const professor = professors.filter((professor) => professor.type === 2 || professor.type === 3);
+
   return (
-    <div className="text-2xl font-semibold">Profesores</div>
-  )
+    <div className="container -my-10 mx-auto py-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-25">
+        {professor.map((professor) => (
+          <InformationCard
+            key={professor.id}
+            name={professor.name.toUpperCase()}
+            position={professor.position}
+            description={professor.description}
+            imageUrl={professor.imageUrl}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
